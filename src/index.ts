78938e8ecd980120e5863309c162c6989391de92
Gustavo Fonseca <gustavofonsec94@gmail.com>
2021-10-04 07:20:14 -0300

    add unworking dev env

--- src/index.ts
+++ src/index.ts
@@ -9,15 +9,40 @@ import recaptcha from '@/recaptcha'
 import ses from '@/ses'
 import { EventBody } from '@/types'
 
+// TODO - Handle dev env variables
+const throwEnvError = (varName: string) => {
+  throw new Error(`Env variable "${varName}" is not set.`)
+}
+
+if (!process.env.RECAPTCHA_SECRET) {
+  throwEnvError('RECAPTCHA_SECRET')
+}
+
+if (!process.env.RECAPTCHA_SCORE_THRESHOLD) {
+  throwEnvError('RECAPTCHA_SCORE_THRESHOLD')
+}
+
+if (!process.env.EMAIL_SOURCE) {
+  throwEnvError('EMAIL_SOURCE')
+}
+
+if (!process.env.EMAIL_DEST) {
+  throwEnvError('EMAIL_DEST')
+}
+
+if (!process.env.EMAIL_SUBJECT) {
+  throwEnvError('EMAIL_SUBJECT')
+}
+
 const env = {
   recaptcha: {
-    SECRET: process.env.RECAPTCHA_SECRET || 'dom_secret',
-    SCORE_THRESHOLD: Number(process.env.RECAPTCHA_SCORE_THRESHOLD) || 0.5,
+    SECRET: process.env.RECAPTCHA_SECRET as string,
+    SCORE_THRESHOLD: Number(process.env.RECAPTCHA_SCORE_THRESHOLD),
   },
   email: {
-    SOURCE: process.env.EMAIL_SOURCE || 'gmtfonseca@gmail.com',
-    DEST: process.env.EMAIL_DEST || 'gustavofonseca94@gmail.com',
-    SUBJECT: process.env.EMAIL_SUBJECT || 'Solicitação de orçamento',
+    SOURCE: process.env.EMAIL_SOURCE as string,
+    DEST: process.env.EMAIL_DEST as string,
+    SUBJECT: process.env.EMAIL_SUBJECT as string,
   },
 }
 
